42280e3b3a6a9670682557ccb7efcee28f8e8545
Scott Morgan <smorgan@bridgend.ac.uk>
2020-04-27 16:10:34 +0100

    Adds tests

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -4,4 +4,4 @@ from src.exercise import main
 def test_exercise(capsys):
     main()
     out, err = capsys.readouterr()
-    assert out == "\n", "Should read ''"
+    assert out == "Once upon a time\nthere was\na program\n", "Should read:\n 'Once upon a time\nthere was\na program\n'"
